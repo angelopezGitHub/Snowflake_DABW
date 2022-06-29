eee006c666f59c9240378de630d668eec8cc7e4a
Angel Lopez <108378082+angelopezGitHub@users.noreply.github.com>
2022-06-29 18:16:40 -0500

    Update streamlit_app.py
    testing emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Parents New Healty Diner')
 streamlit.header('This is how it starts with streamlit/python/snowflake')
 
 streamlit.subheader('Menu')
-streamlit.text('Tuna Sandwich')
+streamlit.text(":shark:","Tuna Sandwich")
 streamlit.text('chicken breast Sandwich')
 streamlit.text('Tuna Salad')
 
