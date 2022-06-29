f8e4060196c226eb6281570cb7cc80d526305f18
Angel Lopez <108378082+angelopezGitHub@users.noreply.github.com>
2022-06-28 21:10:40 -0500

    Update streamlit_app.py
    adding more line to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Diner')
+streamlit.header('This is how it starts with streamlit/python/snowflake')
+
+streamlit.subheader('Menu')
+streamlit.text('Tuna Sandwich')
+streamlit.text('chicken breast Sandwich')
+streamlit.text('Tuna Salad')
