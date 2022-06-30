e684a04cd864540f0c2a5a77c20a48d8225b1f43
Angel Lopez <108378082+angelopezGitHub@users.noreply.github.com>
2022-06-29 19:05:47 -0500

    Update streamlit_app.py
    adding list from file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('This is how it starts with streamlit/python/snowflake')
@@ -16,3 +17,7 @@ streamlit.text('🥑🍞 Avocato Toast')
 
 #new tittle
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#getting file from site and show it
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
